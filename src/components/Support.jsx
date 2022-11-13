d20b1f6cd9c90fac3c87f6d6a54b4e45b12d4b84
MeirJC <110486620+MeirJC@users.noreply.github.com>
2022-11-13 11:35:35 +0000

    0103MJC

---
+++ src/components/Support.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default () => {
+  const [count, setCount] = useState(0)
+  return (
+    <button className="outline" onClick={() => setCount(count + 1)}>
+      {/* {(() => {
+        if (count === 0) {
+          return "Click to support"
+        } else {
+          return `Supported ${count} times`
+        }})()} */}
+      {count === 0 ? "Click to support" : `Supported ${count} times`}
+    </button>
+  )
+}
